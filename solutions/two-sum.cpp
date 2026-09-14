2195ebf3f5135532e3659391057e290ba776dc1c
Anmol Nagpal <150334979+anmolnagpal18@users.noreply.github.com>
2026-09-14 18:02:09 +0530

    ✅ Two Sum (Easy) - [CPP]

---
+++ solutions/two-sum.cpp
@@ -0,0 +1,11 @@
+/**
+ * Problem: Two Sum (Easy)
+ * Language: cpp
+ *
+ * Description:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ */
+
+// Test solution commit
+#include <iostream>
+int main() { return 0; }
